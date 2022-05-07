93aa12a89edb4d964b0ce478d9e160cfe364bebc
Alysson-Araujo <alysson10araujo@gmail.com>
2022-05-07 00:05:59 -0300

    Dia 3 completo

--- frontend/web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ frontend/web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,7 +1,9 @@
 import { ArrowLeft } from "phosphor-react"
 import { FormEvent, useState } from "react"
 import { FeedbackType, feedbackTypes } from ".."
+import { api } from "../../../lib/api"
 import { CloseButton } from "../../CloseButton"
+import { Loading } from "../../Loading"
 import { ScreenshotButton } from "../ScreenshotButton"
 
 interface FeedbackContentStepProps {
@@ -15,17 +17,25 @@ export function FeedbackContentStep({
 }: FeedbackContentStepProps) {
   const [screenshot, setScreenshot] = useState<string | null>(null)
   const [comment, setComment] = useState('')
+const [isSendingFeedback,setIsSedingFeedback] = useState(false)
 
   const feedbackTypeInfo = feedbackTypes[feedbackType]
 
   function handleSubmitFeedback(event: FormEvent) {
     event.preventDefault();
 
-    console.log({
-      screenshot,
-      comment
-    });
-
+    setIsSedingFeedback(true);
+    // console.log({
+      //   screenshot,
+      //   comment
+      // });
+      api.post('/feedbacks',{
+        type: feedbackType,
+        comment,
+        screenshot,
+      })
+      
+      setIsSedingFeedback(false);
     onFeedbackSent()
   }
 
@@ -63,10 +73,10 @@ export function FeedbackContentStep({
 
           <button
             type="submit"
-            disabled={comment.length === 0}
+            disabled={comment.length === 0 || isSendingFeedback }
             className="p-2 bg-brand-500 font-medium rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
           >
-            Enviar feedback
+            {isSendingFeedback ? <Loading /> : 'Enviar feedback'}
           </button>
         </footer>
       </form>
